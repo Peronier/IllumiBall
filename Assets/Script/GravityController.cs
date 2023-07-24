b6bd9d3a63c739d3293214749d817cc26277ea82
perochi <s.shumpei@outlook.jp>
2023-07-24 20:54:43 +0900

    7/24

---
+++ Assets/Script/GravityController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+
+    //重力加速度 
+    const float Gravity = 9.81f;
+
+    //重力の適用具合
+    public float gravityScale = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vector = new Vector3();
+
+        if (Application.isMobilePlatform)
+        {
+            vector.x = Input.acceleration.x;
+            vector.z = Input.acceleration.z;
+            vector.y = Input.acceleration.y;
+        }
+        else
+        {
+            //キーの入力を検知してベクトルを設定
+            vector.x = Input.GetAxis("Horizotnal");
+            vector.z = Input.GetAxis("Vertical");
+
+            //高い方向の判定はキーのzとする
+            if (Input.GetKey("z"))
+            {
+                vector.y = 1.0f;
+            }
+            else
+            {
+                vector.y = -1.0f;
+            }
+        }
+
+        //シーンの重力を入力ベクトルの方向に合わせて変化させる
+        Physics.gravity = Gravity * vector.normalized * gravityScale;
+    }
+}
